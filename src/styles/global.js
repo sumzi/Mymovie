5c8a86f01c6fece256e9a7167cd277d964bfb5f9
sumzi <wltn1873@gmail.com>
2021-04-07 21:00:26 +0900

    Add style configuation

---
+++ src/styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  div {
+    box-sizing: border-box;
+  }
+
+`;
+
+export default GlobalStyle;
