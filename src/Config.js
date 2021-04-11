418c43593f7255180c99ef419143060a6d0fd8bb
sumzi <wltn1873@gmail.com>
2021-04-11 23:56:31 +0900

    Update home page background image

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
 export const API_URL='https://api.themoviedb.org/3/';
-export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/w200/';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/';
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,22 +4,40 @@ import { API_KEY } from '../../key';
 import { API_URL, IMAGE_BASE_URL } from '../../Config';
 import styled from 'styled-components';
 
+const BgImg = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  background-size: cover;
+  height: 100vh;
+  background: ${({ bgPath }) => `url(${bgPath}) center no-repeat`};
+`;
+
+
+
 
 function Home() {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
+    const num = Math.floor(Math.random()*20);
     async function getMovie() {
-      await axios.get(`${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`)
-      .then((res)=>console.log(res.data.results));
+      try {
+        const movies = await axios.get(`${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR`);
+        console.log(movies.data.results[num]);
+        setMovie(movies.data.results[num]);
+      }catch(error){
+        console.log(error);
+      }
     };
     getMovie();
   }, []);
 
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <BgImg bgPath={`${IMAGE_BASE_URL}w1280${movie.backdrop_path}`}/>
+    </>
   )
 }
 
