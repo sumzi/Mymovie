18944c90a6c8953a0a861cd1f5587bb833c37bc0
sumzi <wltn1873@gmail.com>
2021-04-13 23:36:28 +0900

    Update style

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,10 +5,26 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     overflow: hidden;
+    background-color:black;
   }
 
   div {
     box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 10px;
+    right: 0;
+    left: 0;
+    font-size: 12px;
+    text-align: center;
+    color: gray;
+    opacity: 0.5;
   }
 
 `;
