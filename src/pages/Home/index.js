64177ec50bf2192139bb47d31a807e3580facfaa
sumzi <wltn1873@gmail.com>
2021-04-13 23:35:26 +0900

    Update Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,31 +2,22 @@ import React,{ useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_KEY } from '../../key';
 import { API_URL, IMAGE_BASE_URL } from '../../Config';
-import styled from 'styled-components';
-
-const BgImg = styled.div`
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  background-size: cover;
-  height: 100vh;
-  background: ${({ bgPath }) => `url(${bgPath}) center no-repeat`};
-`;
-
-
-
+import { Sub } from '../../components/Nav';
+import { BackgroundImg } from './Home.styled';
 
 function Home() {
   const [movie, setMovie] = useState([]);
-
+  
   useEffect(() => {
     const num = Math.floor(Math.random()*20);
+    // const num = 2;
     async function getMovie() {
       try {
         const movies = await axios.get(`${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR`);
         console.log(movies.data.results[num]);
         setMovie(movies.data.results[num]);
+        const about = await axios.get(`${API_URL}movie/${movie.id}?api_key=${API_KEY}&language=ko-KR`);
+        console.log(about.data);
       }catch(error){
         console.log(error);
       }
@@ -36,7 +27,12 @@ function Home() {
 
   return (
     <>
-      <BgImg bgPath={`${IMAGE_BASE_URL}w1280${movie.backdrop_path}`}/>
+      <BackgroundImg bgPath={`${IMAGE_BASE_URL}w1280${movie.backdrop_path}`}>
+        <Sub />
+        <div>
+          하하ㅏㅎ
+        </div>
+      </BackgroundImg>
     </>
   )
 }
