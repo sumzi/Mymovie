60977b44e83034b609c574824f0859535fad79f5
sumzi <wltn1873@gmail.com>
2021-04-15 01:28:40 +0900

    Add store

---
+++ src/store/reducers/movie.js
@@ -0,0 +1,36 @@
+import produce from 'immer';
+
+const initialState = {
+  movies: [],
+  loadMoviesLoading: false,
+  loadMoviesDone: false,
+  loadMoviesError: null,
+  homeMovie: null,
+};
+
+export const LOAD_MOVIES_REQUEST = 'LOAD_MOVIES_REQUEST';
+export const LOAD_MOVIES_SUCCESS = 'LOAD_MOVIES_SUCCESS';
+export const LOAD_MOVIES_FAILURE = 'LOAD_MOVIES_FAILURE';
+
+const reducer = (state = initialState, action) => produce(state, (draft) => {
+  switch (action.type) {
+    case LOAD_MOVIES_REQUEST:
+      draft.loadMoviesLoading = true;
+      draft.loadMoviesDone = false;
+      draft.loadMoviesError = null;
+      break;
+    case LOAD_MOVIES_SUCCESS:{
+      draft.loadMoviesLoading = false;
+      draft.loadMoviesDone = true;
+      draft.homeMovie = action.data;
+      break;}
+    case LOAD_MOVIES_FAILURE:
+      draft.loadMoviesLoading = false;
+      draft.loadMoviesError = action.error;
+      break;
+    default:
+      break;
+  };
+});
+
+export default reducer;
---
+++ src/store/sagas/movie.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { all, fork, put, takeLatest, call } from 'redux-saga/effects';
+import {
+  LOAD_MOVIES_REQUEST,
+  LOAD_MOVIES_SUCCESS,
+  LOAD_MOVIES_FAILURE,
+} from '../reducers/movie';
+import { API_URL, API_KEY } from '../../Config';
+
+
+function loadMoviesAPI(data){
+  return axios.get(`${API_URL}movie/${data}?api_key=${API_KEY}&language=ko-KR`);
+}
+
+function getMovieAPI(data){
+  console.log(data.id)
+  return axios.get(`${API_URL}movie/${data.id}?api_key=${API_KEY}&language=ko-KR`);
+}
+
+
+function* loadMovies(action) {
+  try{
+    const tmp = yield call(loadMoviesAPI, action.data);
+    const num = Math.floor(Math.random()*20);
+    const result = yield call(getMovieAPI, tmp.data.results[num]);
+    console.log(result);
+    yield put({
+      type: LOAD_MOVIES_SUCCESS,
+      data: result.data,
+    });
+  } catch(err) {
+    console.error(err);
+    yield put({
+      type: LOAD_MOVIES_FAILURE,
+      error: err.response.data,
+    })
+  }
+}
+
+function* watchLoadMovies() {
+  yield takeLatest(LOAD_MOVIES_REQUEST, loadMovies);
+}
+
+export default function* movieSaga() {
+  yield all([
+    fork(watchLoadMovies),
+  ])
+}
