d1e37e870a94d4f029d148b52efd62e23ea2da7a
sumzi <wltn1873@gmail.com>
2021-04-18 00:45:41 +0900

    Add style variables

---
+++ src/styles/variables.js
@@ -0,0 +1,12 @@
+export const primaryColor = 'black';
+export const secondaryColr = 'white';
+
+const size = {
+  mobile: '480px',
+  tablet: '768px',
+};
+
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+}
