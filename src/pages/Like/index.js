735f342ef6094cab6259e3358325782bd1849797
sumzi <wltn1873@gmail.com>
2021-04-18 00:44:28 +0900

    Add like page

--- src/pages/Like/index.js
+++ src/pages/Like/index.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Empty  } from 'antd';
+
 
 function Like() {
+  const { likes } = useSelector(state => state.movie)
   return (
     <div>
+      {/*  likes 가 없으면 ㅎㅎ */}
+      {likes?(<div>있네여</div>)
+      :<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />}
       
     </div>
   )
