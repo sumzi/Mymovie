7b9f38e698c9770abff3ada23bdcf3ea0911e68a
sumzi <wltn1873@gmail.com>
2021-04-19 01:16:04 +0900

    Update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,6 @@
 import React,{ useEffect } from 'react';
 import { IMAGE_BASE_URL } from '../../Config';
-import { Sub } from '../../components/Nav';
-import { BackgroundImg, Detail, Wrapper } from './Home.styled';
+import { BackgroundImg, Detail } from './Home.styled';
 import { HOME_MOVIE_REQUEST } from '../../store/reducers/home';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../../components/Loading';
@@ -20,13 +19,13 @@ function Home() {
     <>
       {homeMovieLoading? 
       (<Loading />)
-      : (<BackgroundImg bgPath={`${IMAGE_BASE_URL}w1280${homeMovie.backdrop_path}`}>
+      : <BackgroundImg bgPath={`${IMAGE_BASE_URL}w1280${homeMovie.backdrop_path}`}>
         <Detail>
           {homeMovie.tagline ? <p>"{homeMovie.tagline}"</p>:<p></p>}
           <p>🎞 {homeMovie.title}</p>
         </Detail>
       </BackgroundImg>
-      )}
+      }
     </>
   )
 }
