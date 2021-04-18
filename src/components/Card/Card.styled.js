ea6278d7778773f0e60d917637d7e8b2df814c31
sumzi <wltn1873@gmail.com>
2021-04-18 22:54:11 +0900

    Update Card component design

--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -1,11 +1,23 @@
 import styled from 'styled-components';
+import { device } from '../../styles/variables';
 
 const CardWrapper = styled.div`
   position: relative;
-  a img {
+  padding: 0 30px;
+  @media ${device.tablet} {
+    padding: 20px;
+  }
+  img {
+    border-radius: 15px;
     width: 100%;
-    height: 320px;
+    height: 100%;
+    box-shadow: 0px 5px 5px black;
+    &:hover {
+      transform: scale(1.1);   
+      transition: 1s;
+    }
   }
 `;
 
-export { CardWrapper }
+
+export { CardWrapper };
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { Col, Image }from 'antd';
-import { CardWrapper } from './Card.styled';
+import React from "react";
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Col, Image } from "antd";
+import { CardWrapper } from "./Card.styled";
+import { BACKGROUND_IMAGE_REQUEST } from '../../store/reducers/movie';
+
 function Card(movie) {
+  const dispatch = useDispatch();
+
+  const mouseEnter = () => {
+    console.log(movie.movieTitle);
+    dispatch({
+      type: BACKGROUND_IMAGE_REQUEST,
+      data: movie.movieBgImg
+    })
+  };
+
+  const movieClick = () => {
+
+  }
+  
   return (
-    <Col lg={6} md={8} xs={24}>
-      <CardWrapper>
-        <a href={`/movie/${movie.movieId}`}>
-          {movie.image ?
-          <img src={movie.image} alt={movie.movieTitle}/>:
-          <Image src="err"/>}
-        </a>
-      </CardWrapper>
-      
-    </Col>
-  )
+      <Col lg={6} md={8} xs={24}>
+        <CardWrapper>
+          <Link to={`/about/${movie.movieId}`}>
+            {movie.image ? (
+              <img src={movie.image} alt={movie.movieTitle} onMouseEnter={mouseEnter} onClick={movieClick}/>
+            ) : (
+              <Image src="err" />
+            )}
+          </Link>
+        </CardWrapper>
+      </Col>
+  );
 }
 
-export default Card
+export default Card;
