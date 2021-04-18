7f14c6f83eae7f09e5b30e8b4bb492f53046b9cf
sumzi <wltn1873@gmail.com>
2021-04-19 01:17:26 +0900

    Update running page

---
+++ src/pages/Running/index.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { IMAGE_BASE_URL } from "../../Config";
+import Sub from '../../components/Nav/Sub';
+import { useDispatch, useSelector } from "react-redux";
+import { LOAD_MOVIES_REQUEST } from "../../store/reducers/movie";
+import { BackgroundImg, MovieWrapper } from "./Running.styled";
+import { Row } from "antd";
+import Card from '../../components/Card';
+
+function Movie() {
+  const dispatch = useDispatch();
+  const { movies, movieType, backgroundImage } = useSelector((state) => state.movie);
+
+  useEffect(() => {
+    if (movieType === "") {
+      dispatch({
+        type: LOAD_MOVIES_REQUEST,
+        data: "0",
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <Sub />
+      {backgroundImage && <BackgroundImg bgPath={`${IMAGE_BASE_URL}w1280${backgroundImage}`}/>}
+      <MovieWrapper>
+        <Row gutter={[16, 16]}>
+          {movies &&
+            movies.map((movie, index) => (
+              <Card
+                key={index}
+                image={
+                  movie.poster_path
+                    ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                    : null
+                }
+                movieId={movie.id}
+                movieTitle={movie.title}
+                movieBgImg={movie.backdrop_path}
+              />
+            ))}
+        </Row>
+      </MovieWrapper>
+    </>
+  );
+}
+
+export default Movie;
