5b0fba4ccc77b196339cbfbca54b20c9fcd1c004
sumzi <wltn1873@gmail.com>
2021-04-20 16:52:19 +0900

    Update nav css design

--- src/components/Nav/Main/Main.styled.js
+++ src/components/Nav/Main/Main.styled.js
@@ -3,44 +3,49 @@ import { device } from '../../../styles/variables';
 
 const MainWrapper = styled.div`
   height: 60px;
+  position: fixed;
+  top: 0;
   display: flex;
   justify-content: space-between;
   background-color: black;
-  padding: 0 20px;
+  padding: 0 30px;
   @media ${device.tablet} {
     padding: 0 50px;
+    height: 70px;
+    div {
+      line-height: 70px;
+    }
   }
+  z-index: 30;
+  width: 100%;
 `;
 
 const LeftWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  //border: 2px solid blue;
   line-height: 60px;
 `;
 const RightWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  //border: 2px solid red;
-  width: 120px;
+  width: 140px;
   @media ${device.tablet} {
-    width: 140px;
+    width: 180px;
   }
   div {
     color: white;
-    font-size: 25px;
+    font-size: 27px;
     line-height: 60px;
     @media ${device.tablet} {
-      font-size: 30px;
+      font-size: 35px;
+      line-height: 70px;
     }
-    //border: 2px solid blue;
   }
-  z-index: 20;
 `;
 const Logo = styled.div`
-  width: 70px;
+  width: 80px;
   @media ${device.tablet} {
-    width: 100px;
+    width: 120px;
   }
   img {
     width: 100%;
--- src/components/Nav/Sub/Sub.styled.js
+++ src/components/Nav/Sub/Sub.styled.js
@@ -2,32 +2,33 @@ import styled from 'styled-components';
 import { device } from '../../../styles/variables';
 
 const SubWrapper = styled.div`
+  margin-top: 60px;
   display: flex;
   justify-content: space-around;
   flex-flow: row wrap;
-  //border: 1px solid blue;
-  height: 40px;
-  width: 100 %;
-  margin: 0 auto;
-  line-height: 40px;
+  height: 50px;
+  width: 100%;
+  line-height: 50px;
   @media ${device.tablet} {
-    padding: 0 120px;
+    margin-top: 70px;
+    padding: 0 100px;
+    height: 50px;
+    line-height: 50px;
   }
-  z-index: 20;
 `;
 
 const Menu = styled.div`
-  //border: 2px solid red;
-  z-index: 10;
+  z-index:10;
   cursor: pointer;
   color: ${props => props.flag?'white':'gray'};
-  font-size: 16px;
+  font-weight: bold;
+  font-size: 19px;
   &:hover {
     transform: scale(1.15);
     transition: .2s;
   }
   @media ${device.tablet} {
-    font-size: 22px;
+    font-size: 28px;
   }
 `;
 
