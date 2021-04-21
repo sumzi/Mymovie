84dc9bbf076091fdacdfce36f05567b4027534c8
sumzi <wltn1873@gmail.com>
2021-04-21 23:31:50 +0900

    Update store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import movie from './movie';
 import home from './home';
 import about from './about';
+import search from './search';
 
 const rootReducer = combineReducers({
   movie,
   home,
   about,
+  search,
 });
 export default rootReducer;
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,12 +2,13 @@ import { all, fork } from 'redux-saga/effects';
 import movieSaga from './movie';
 import homeSaga from './home';
 import aboutSaga from './about';
-
+import searchSaga from './search';
 
 export default function* rootSaga() {
   yield all([
     fork(movieSaga),
     fork(homeSaga),
-    fork(aboutSaga),
+    fork(aboutSaga),    
+    fork(searchSaga),
   ]);
 }
