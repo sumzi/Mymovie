418c7164f4f8acb3a6dd77a96045ddd767341aac
sumzi <wltn1873@gmail.com>
2021-05-03 23:20:03 +0900

    Update footer component css design

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { GithubOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import { device } from '../../styles/variables';
 
 const FooterWrapper = styled.div`
-  color: gray;
-  height: 100px;
-  line-height: 100px;
+  height: 60px;
+  line-height: 60px;
   width: 100%;
   text-align: center;
-  opacity: 0.4;
-  font-size: 17px;
+  font-size: 12px;
   label {
     margin: 0 9px;
   }
   a{
-    color: white;
+    color: gray;
   }
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
+  @media ${device.tablet}{
+    font-size: 17px;
+    height: 100px;
+    line-height: 100px;
+  }
 `;
 
 function Footer() {
